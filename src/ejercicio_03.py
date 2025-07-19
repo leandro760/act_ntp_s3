94118e635fbbcfe13363ec2bca77fd2a5e8a050e
leandro <lacevedoal93@gmail.com>
2025-07-18 20:23:26 -0500

    solucion ejercicio 3, 4

---
+++ src/ejercicio_03.py
@@ -0,0 +1,7 @@
+# Con un ciclo for, calcula la suma de todos los enteros del 1 al 100
+#  (inclusive) y muestra el resultado final.
+
+suma = 0
+for i in range(1, 101):
+    suma += i
+print(suma)  
---
+++ src/ejercicio_04.py
@@ -0,0 +1,13 @@
+# Utilizando un ciclo while, solicita al usuario que ingrese números. 
+# El proceso termina cuando el usuario escriba 0. Al final, 
+# muestra la suma total de todos los números ingresados.
+
+suma = 0
+
+while True:
+  numero = input("Ingrese un número, (0 para terninar): ")
+  if numero == "0":
+    break
+  suma += float(numero)
+  print("La suma total de los ingresados es: ", suma)
+    
